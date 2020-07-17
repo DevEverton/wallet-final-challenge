6cbf12c45f0c4d24b3e73ad99af04bec41a104c5
Everton Carneiro <everton.mercante@gmail.com>
2020-07-17 18:08:48 -0300

    Implemented states and transactionService

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import transactionDataService from "../src/services/TransactionService.js";
 
 export default function App() {
-  const getTransactions = (period) => {
-    transactionDataService
-      .getByPeriod(period)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const [transactions, setTransactions] = useState({});
+  const [period, setPeriod] = useState("");
+
+  useEffect(() => {
+    const date = new Date();
+    getTransactions("2020-08");
+    setPeriod(`${date.getFullYear()}-${date.getMonth() + 1}`);
+  }, []);
+
+  const getTransactions = async (period) => {
+    try {
+      const transactionsByPeriod = await transactionDataService.getByPeriod(
+        period
+      );
+      setTransactions(transactionsByPeriod.data);
+    } catch (err) {
+      console.log(err);
+    }
   };
+  console.log(period);
 
   return (
     <div>
       <h3>Desafio Final do Bootcamp Full Stack</h3>
-      <button
-        className="badge badge-danger mr-2"
-        onClick={getTransactions("2020-08")}
-      >
-        GetAll
-      </button>
+      <p>{JSON.stringify(transactions)}</p>
     </div>
   );
 }
--- client/src/services/TransactionService.js
+++ client/src/services/TransactionService.js
@@ -4,4 +4,15 @@ const getByPeriod = (period) => {
   return http.get(`/api/transaction?period=${period}`);
 };
 
-export default { getByPeriod };
+const add = () => {
+  return http.post("/api/transaction");
+};
+
+const edit = (id) => {
+  return http.put(`/api/transaction/${id}`);
+};
+
+const remove = (id) => {
+  return http.delete(`/api/transaction/${id}`);
+};
+export default { getByPeriod, add, edit, remove };
