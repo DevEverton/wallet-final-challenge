02c7d2236e9566dd3e9bf568fd541349a2d56767
Everton Carneiro <everton.mercante@gmail.com>
2020-07-18 13:51:26 -0300

    Setup basic transaction card layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import transactionDataService from "../src/services/TransactionService.js";
+import TransactionCard from "./components/TransactionCard.js";
 
 export default function App() {
   const [transactions, setTransactions] = useState({});
@@ -7,9 +8,16 @@ export default function App() {
 
   useEffect(() => {
     const date = new Date();
-    getTransactions("2020-08");
-    setPeriod(`${date.getFullYear()}-${date.getMonth() + 1}`);
+    const yearMonthStr =
+      date.getMonth() + 1 < 10
+        ? `${date.getFullYear()}-0${date.getMonth() + 1}`
+        : `${date.getFullYear()}-${date.getMonth() + 1}`;
+    setPeriod(yearMonthStr);
   }, []);
+
+  useEffect(() => {
+    getTransactions(period);
+  }, [period]);
 
   const getTransactions = async (period) => {
     try {
@@ -22,11 +30,16 @@ export default function App() {
     }
   };
   console.log(period);
+  console.log(transactions);
 
   return (
     <div>
-      <h3>Desafio Final do Bootcamp Full Stack</h3>
-      <p>{JSON.stringify(transactions)}</p>
+      <div className="container right">
+        <h3>Desafio Final do Bootcamp Full Stack</h3>
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+      </div>
     </div>
   );
 }
